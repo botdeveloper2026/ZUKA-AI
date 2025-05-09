bd9410d6ada67ca2216b69fced925eaa7c54649c
botdeveloper2026 <heyyougangster@gmail.com>
2025-05-09 14:02:01 +0200

    Add files via upload

---
+++ zuka-ai.js
@@ -0,0 +1,20 @@
+var commands = [];
+
+function zuka-ai(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    zuka-ai,
+    AddCommand:zuka-ai,
+    Function:zuka-ai,
+    Module:zuka-ai,
+    commands,
+};
